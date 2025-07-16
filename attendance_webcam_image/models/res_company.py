fd29129aa0aa6aaf10e52591201807d48d1ce338
ssgumera23 <41278758+ssgumera23@users.noreply.github.com>
2025-07-16 23:30:25 +0800

    Delete attendance_webcam_image directory

--- attendance_webcam_image/models/res_company.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models, api
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-    
-    attendance_webcam_image = fields.Boolean(string="Attendances Webcam Image", default=False)
--- attendance_webcam_image/models/res_config_settings.py
+++
@@ -1,6 +0,0 @@
-from odoo import api, fields, models
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-    
-    attendance_webcam_image = fields.Boolean(related="company_id.attendance_webcam_image", string="Attendances Webcam Image", readonly=False)
